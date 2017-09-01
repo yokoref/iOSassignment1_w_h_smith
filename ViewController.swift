f987be8af8d3732292ed2e87c7cbc11efe7136e8
umslarcher <umslarcher@gmail.com>
2017-09-01 05:46:26 -0500

    one more againnl

---
+++ ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by William Smith on 8/30/17.
+//  Copyright © 2017 William Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    
+    @IBAction func btnOne(_ sender: Any) {
+    }
+    
+    @IBAction func btnTwo(_ sender: Any) {
+    }
+    
+    @IBAction func doneBtnPressed(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ aboutMe.swift
@@ -0,0 +1,9 @@
+//
+//  aboutMe.swift
+//  assignment1
+//
+//  Created by William Smith on 8/30/17.
+//  Copyright © 2017 William Smith. All rights reserved.
+//
+
+import Foundation
